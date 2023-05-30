e7f118e2b40c72e7dcee10c112188eb5b9250070
Breanna Gibbons <99208900+bregibbons@users.noreply.github.com>
2023-05-30 11:08:54 -0400

    Started working on the player class

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,30 @@
 import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		Deck d= new Deck();
+		Deck d = new Deck();
 		d.shuffle();
-		ArrayList<Card> P1=new ArrayList<>();
-		ArrayList<Card> P2=new ArrayList<>();
-		ArrayList<Card> P3=new ArrayList<>();
-		ArrayList<Card> P4=new ArrayList<>();
-		
-		for (int x=0;x<8;x++) {
-			P1.add(d.deal());
+		ArrayList<Card> Player = new ArrayList<>();
+		ArrayList<Card> P2 = new ArrayList<>();
+		ArrayList<Card> P3 = new ArrayList<>();
+		ArrayList<Card> P4 = new ArrayList<>();
+
+		for (int x = 0; x < 8; x++) {
+			Player.add(d.deal());
 			P2.add(d.deal());
 			P3.add(d.deal());
 			P4.add(d.deal());
 
 		}
-		System.out.println();
+
+		System.out.println("Welcome to crazy 8s");
+		System.out.println("the cards have been shuffled");
+		System.out.println("you have "++" in your hand");
+
+	}
+
 	}
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,30 @@
 import java.util.*;
-public class Player {
 
+public class Player {
+	String hand;
+	ArrayList<Card> player = new ArrayList<>();
 	
+	
+	public Player (String hand1, ArrayList<Card> player1) {
+		hand = hand1;
+		player = player1;
+		
+	}
+	
+	ArrayList<Card> deal() {
+		for (int x = 0; x < 8; x++) {
+			player.add(d.deal());
+		}
+		return player;
+		
+	}
+	
+	String getHand() {
+		for (int x = 0; x < player.size(); x++) {
+			
+		}
+	return hand;
+	}
+	
+
 }
