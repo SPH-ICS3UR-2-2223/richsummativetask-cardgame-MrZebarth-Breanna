6da562db4e878ca9dabd85a78a8331ae8f787251
Breanna Gibbons <99208900+bregibbons@users.noreply.github.com>
2023-06-06 11:16:19 -0400

    Finished the person's turn and started working on the bots

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,7 @@ public class Main {
 		int round = 1;
 		int player = 2;
 		int pos = 0;
+		int turns = 0;
 
 		for (int x = 0; x < 8; x++) {
 			P1.add(d.deal());
@@ -35,20 +36,92 @@ public class Main {
 		System.out.println("Welcome to crazy 8s");
 		System.out.println("how many rounds would you like to play?");
 		int rouNum = in.nextInt();
+		in.nextLine();
 
 		if (round <= rouNum) {
+		
+		while (turns<9) {
 
 			System.out.println("the cards have been shuffled");
 
-			discard.add(d.deal());
-
-			System.out.println("the top card has been flipped to reveal " + d.getName(pos) + "");
 			System.out.println("You go first");
 
-			if (turn > 1) {
-				
+			while (turn == 1) {
+				System.out.println("the top card is " + d.getName(pos) + "");
+				discard.add(d.deal());
+				System.out.println("you have " + P1 + " in your hand");
+				boolean correct = true;
 
-				System.out.println("P" + player + " played... ");
+				while (correct) {
+					System.out.println(
+							"What card would you like to play? (please select the number position of your card or 0 to pick up)");
+					int carChoice = in.nextInt();
+					in.nextLine();
+					
+					carChoice = carChoice - 1;
+
+					if (carChoice == -1) {
+						P1.add(d.deal());
+						turn = turn + 1;
+						correct = false;
+						
+					} else if (P1.get(carChoice).getNumber() == 8){
+						System.out.println("You put a CRAZY 8! What would you like to change the suit to?");
+						String newSuit = in.nextLine();
+						newSuit.toUpperCase();
+
+						if (newSuit.startsWith("S")){
+							
+						} else if (newSuit.startsWith("C")) {
+							P1.get(carChoice).getSuit();
+							
+						} else if (newSuit.startsWith("D")) {
+							
+							
+						} else if (newSuit.startsWith("H")) {
+							
+							
+						}
+							
+						
+						discard.add(P1.get(carChoice));
+						
+					} else {
+
+						if (P1.get(carChoice).getSuit() == discard.get(discard.size() - 1).getSuit()
+								|| P1.get(carChoice).getNumber() == discard.get(discard.size() - 1).getNumber()) {
+
+							discard.add(P1.get(carChoice));
+
+							System.out.println("You put " + discard.get(discard.size() - 1) + "");
+
+							turn = turn + 1;
+							correct = false;
+						} else {
+							System.out.println("you can't put that card pick another");
+
+						}
+
+					}
+				}
+			}
+
+			while (turn > 1) {
+				
+				for (int x = 0; x < P2.size(); x++) {
+					
+					if (P2.get(x).getSuit() == discard.get(discard.size() - 1).getSuit()
+								|| P2.get(x).getNumber() == discard.get(discard.size() - 1).getNumber()) {
+						
+						System.out.println("P" + player + " played");
+						break;
+						
+					} else {
+						P2.add(d.deal());
+						System.out.println("P" + player + " skipped");
+					}
+				}
+
 				if (turn < 4) {
 					turn++;
 					player++;
@@ -57,33 +130,11 @@ public class Main {
 					player = 2;
 				}
 
-			} else if (turn == 1) {
-				System.out.println("you have " + P1 + " in your hand");
-				boolean correct = true;
-
-				while (correct) {
-					System.out.println(
-							"What card would you like to play? (please select the number position of your card)");
-					int carChoice = in.nextInt();
-					carChoice--;
-
-					if (P1.get(carChoice).getSuit() == discard.get(0).getSuit() || P1.get(carChoice).getNumber() == discard.get(0).getNumber()) {
-						
-						discard.add(P1.get(carChoice));
-						
-						System.out.println("You put " + discard.get(discard) + "");
-
-						turn++;
-						correct = false;
-					} else {
-						System.out.println("you can't put that card pick another");
-
-					}
-
-				}
 			}
 
 			round++;
+			
+		}
 
 		} else if (round > rouNum) {
 			System.out.println("Thank you for playing!");
--- src/Player.java
+++ src/Player.java
@@ -4,20 +4,11 @@ public class Player {
 	Scanner in = new Scanner(System.in);
 
 	ArrayList<Card> player = new ArrayList<>();
-	Deck d = new Deck();
 
 	// player object
 	public Player(ArrayList<Card> player1) {
 		player = player1;
 
 	}
-
-	//
-//	public String toString() {
-//		return this.hand;
-//	}
-
-	//
-
 
 }
